298078841a7bac2ba07187cf0177905e078acca4
VShawFluenta <Victoria.j.shaw.47@gmail.com>
2024-07-30 22:31:08 -0700

    it prints and access most of the content, but I can't seem to print the lable of the disk, unless it is meant to be funky

--- CSC360/A3/diskinfo.c
+++ CSC360/A3/diskinfo.c
@@ -41,20 +41,33 @@ Figure out how the heck to access a file
 
 #pragma pack(push, 1)
 struct BootSector {
-    uint8_t  BS_jmpBoot[3];
-    uint8_t  BS_OEMName[8];
-    uint16_t BPB_BytsPerSec;
-    uint8_t  BPB_SecPerClus;
-    uint16_t BPB_RsvdSecCnt;
-    uint8_t  BPB_NumFATs;
-    uint16_t BPB_RootEntCnt;
-    uint16_t BPB_TotSec16;
-    uint8_t  BPB_Media;
-    uint16_t BPB_FATSz16;
-    uint16_t BPB_SecPerTrk;
-    uint16_t BPB_NumHeads;
-    uint32_t BPB_HiddSec;
-    uint32_t BPB_TotSec32;
+    uint8_t  BS_jmpBoot[3];//These two are ignored. 
+    uint8_t  BS_OEMName[8];//
+    uint16_t BPB_BytsPerSec;//Bytes per sector
+    uint8_t  BPB_SecPerClus; //sectors per cluster. 
+    uint16_t BPB_RsvdSecCnt;//number of reserved sectors
+    uint8_t  BPB_NumFATs; //number of fats, should be 2 in this case
+    uint16_t BPB_RootEntCnt; //Maximum number of root directory entries (two bytes)
+    uint16_t BPB_TotSec16; //total sector count. this shouls be 16 bits (two bytes)
+    uint8_t  BPB_Media; //don't use this, but keep so that the alignment isn't thrown off
+    uint16_t BPB_FATSz16;//Sectors per FAT, (two bytes)
+    uint16_t BPB_SecPerTrk; //Sectors per track, not sure hwat this is? 
+    uint16_t BPB_NumHeads;//Number of heads
+    // uint8_t  ToIgnore; //bytes 28-36
+
+
+
+    uint32_t BPB_HiddSec; //bytes 28-32, to be ignored. 
+    uint32_t BPB_TotSec32; //Bytes 32-36, to be ignored. 
+    uint8_t  Boot_Signature; //this should be 0x29 (41) if the Volume Id, Volume label, and File System type. 
+    uint32_t VolumeID;//this is only present if Boot signature is correct
+    uint8_t  VolumeLable[11];
+    uint8_t  FileSystemType[8];
+
+    int FileCount ; 
+
+
+
 };
 #pragma pack(pop)
 
@@ -76,14 +89,34 @@ struct DirectoryEntry {
 #pragma pack(pop)
 
 void readBootSector(FILE *fp, struct BootSector *bootSector) {
-    fseek(fp, 0, SEEK_SET);
+    fseek(fp, 0, SEEK_SET); //ADD ERROR MESSAGES/CHECKING TODO
     fread(bootSector, sizeof(struct BootSector), 1, fp);
+    bootSector -> FileCount =0; 
 }
 
 void readRootDirectory(FILE *fp, struct BootSector *bootSector, struct DirectoryEntry *rootDir) {
     int rootDirStart = (bootSector->BPB_RsvdSecCnt + bootSector->BPB_NumFATs * bootSector->BPB_FATSz16) * bootSector->BPB_BytsPerSec;
-    fseek(fp, rootDirStart, SEEK_SET);
+    fseek(fp, rootDirStart, SEEK_SET); //TODO ADD ERROR CHECKING/MESSAGES
     fread(rootDir, sizeof(struct DirectoryEntry), bootSector->BPB_RootEntCnt, fp);
+}
+void PrintFileName(uint8_t nameArray[]){
+       // Null-terminate the name field for safety, though not strictly necessary
+    char name[12];
+    for (int i = 0; i < 11; i++) {
+        name[i] = nameArray[i];
+    }
+    name[11] = '\0';
+
+    // Print the filename and extension
+    printf("File or Dir Name: [%.8s.%.3s]\n", name, name + 8);
+}
+void PrintArray(uint8_t array[], int n){
+    char string[n+1];
+    for (int i = 0; i < n; i++) {
+        string[i] = array[i];
+    }
+    string[n] = '\0';
+    printf("[%s]\n",string); 
 }
 
 
@@ -113,9 +146,54 @@ int main(int argc, char *argv[]) {
         if (rootDir[i].DIR_Name[0] == 0xE5) {
             continue; // Deleted entry
         }
-        printf("%.11s\n", rootDir[i].DIR_Name);
+        PrintFileName(rootDir[i].DIR_Name);
+        // printf("%.11s\n", rootDir[i].DIR_Name);
     }
+    //PRINTING DISKINFO
+    /* for reference
+    struct BootSector {
+    uint8_t  BS_jmpBoot[3];//These two are ignored. 
+    uint8_t  BS_OEMName[8];//
+    uint16_t BPB_BytsPerSec;//Bytes per sector
+    uint8_t  BPB_SecPerClus; //sectors per cluster. 
+    uint16_t BPB_RsvdSecCnt;//number of reserved sectors
+    uint8_t  BPB_NumFATs; //number of fats, should be 2 in this case
+    uint16_t BPB_RootEntCnt; //Maximum number of root directory entries (two bytes)
+    uint16_t BPB_TotSec16; //total sector count. this shouls be 16 bits (two bytes)
+    uint8_t  BPB_Media; //don't use this, but keep so that the alignment isn't thrown off
+    uint16_t BPB_FATSz16;//Sectors per FAT, (two bytes)
+    uint16_t BPB_SecPerTrk; //Sectors per track, not sure hwat this is? 
+    uint16_t BPB_NumHeads;//Number of heads
+    // uint8_t  ToIgnore; //bytes 28-36
 
+
+
+    uint32_t BPB_HiddSec; //bytes 28-32, to be ignored. 
+    uint32_t BPB_TotSec32; //Bytes 32-36, to be ignored. 
+    uint8_t  Boot_Signature; //this should be 0x29 (41) if the Volume Id, Volume label, and File System type. 
+    uint32_t VolumeID;//this is only present if Boot signature is correct
+    uint8_t  VolumeLable[11];
+    uint8_t  FileSystemType[8];*/
+
+    //NOTE TO SELF the 16 bit values might need conversion from big to little Endain, and Hex to binary. 
+
+    printf("This OS is: "); PrintArray(bootSector.FileSystemType,8);
+    printf("The boot signature is %d \n", bootSector.Boot_Signature);
+        printf("The bytes per sector is  %d \n", bootSector.BPB_BytsPerSec);
+
+
+    printf("Lable of this disk: "); PrintArray(bootSector.VolumeLable,11);//MUST ADD CHECKING THAT THIS EXISTS!!
+
+    printf("Total size of the disk: \n");
+    printf("Free size of the disk: \n");
+    printf("==============\n");
+    printf("The number of files in the disk: \n");
+// (including all files in the root directory and files in all subdirectories):
+    printf("==============\n");
+    printf("Number of FAT copies: %d\n", bootSector.BPB_NumFATs);
+    printf("Sectors per FAT: %d\n", bootSector.BPB_FATSz16);
+
+    //clean up
     free(rootDir);
     fclose(pointerToFile);
 
